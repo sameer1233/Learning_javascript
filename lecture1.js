27ebe77ad13465ff6cfbd474da781c7eed095a0a
Sameer Singh <Sameer0Singh123@gmail.com>
2024-06-20 13:14:42 +0530

    lecture1 of javascript completed

---
+++ lecture1.js
@@ -0,0 +1,23 @@
+// Variable , Constent and comments
+// console.log("Hello coders")
+
+/* 
+  Variable ==> Variable are the container that stores that stores the different types of the value 
+*/
+
+// var age = 23;
+// var age = 34;   This is bad practice and we should not reassign the variable 
+// console.log(age)
+
+// That is the reason we use the let variable 
+// let age = 23;
+// let age = 234  As it shows we cannot redeclare the variable but we can the change the value 
+
+// const height = 5.5
+
+// Datatype
+ 
+
+
+
+
